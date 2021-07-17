d7645bfc1e79b91663f54fb8fa00418162d16f82
stslex <ilya977.077@gmail.com>
2021-07-18 00:25:21 +0300

    Change API_KEY visibility

--- app/src/main/java/com/example/testapplicationweather/utilites/const.kt
+++ app/src/main/java/com/example/testapplicationweather/utilites/const.kt
@@ -1,7 +1,9 @@
 package com.example.testapplicationweather.utilites
 
+import com.example.testapplicationweather.BuildConfig
+
 const val BASE_URL = "https://api.darksky.net/"
 const val GET_MSK = "55.753913, 37.620836"
 const val GET_SPB = "59.950015, 30.316599"
-const val API_KEY = "3e7e519ea86c8e3fcf67c0f4870513d7"
+const val API_KEY = BuildConfig.API_KEY
 const val API_GET = "forecast"
